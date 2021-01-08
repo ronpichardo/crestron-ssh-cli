1bf7014d5f8f5f988364cded2c01507cc0b4ab53
ronpichardo <ron@ronpich.com>
2021-01-08 13:04:28 -0500

    Updated code and readme

--- README.md
+++ README.md
@@ -20,9 +20,23 @@ OPTIONS:
 -v, --version - get version of devices provided into the csv file
 -ch, --check-ports - gets the status of ports, returns if the port is on/off for webserver, ftpserver, ctpconsole, telnet
 -cl, --close-ports - close ports if they are on/enabled for ftpserver, ctpconsole, telnet
+-a, --adhoc - run a command passed in as an argument
 ```
 
 FLAG:
 all - a flag set for the close-ports option, to include the shutdown of the webserver
 
+Output example when running, including an IP that created an exception
+<img width="703" alt="SShOutput" src="https://user-images.githubusercontent.com/63974878/104048673-a0934780-51b1-11eb-93aa-862724c03f2b.png">
+
+```shell
+(myvenv) ➜  crestron-ssh-cli git:(main) ✗ python main.py -v devices.csv 
+CP3 Cntrl Eng [v1.501.2867.24563 (Nov 07 2016)
+CP3 Cntrl Eng [v1.601.3935.27221 (Oct 11 2019)
+Unable to connect to 100.112.200.31
+CP3 Cntrl Eng [v1.601.3935.27221 (Oct 11 2019)
+CP3 Cntrl Eng [v1.601.3935.27221 (Oct 11 2019)
+CP3 Cntrl Eng [v1.601.3935.27221 (Oct 11 2019)
+CP3 Cntrl Eng [v1.501.2867.24563 (Nov 07 2016)
+```
 [Not yet implemented] update - an update to the version option, to update hardware if it is out of date
--- main.py
+++ main.py
@@ -17,6 +17,29 @@ remotePath = ''
 sshclient = paramiko.SSHClient()
 sshclient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
+# def upload_puf():
+#   sshclient.connect('100.112.100.230', username='crestron', password='')
+#   stdin, stdout, stderr = sshclient.exec_command('dir romdisk/user/system')
+#   print(stdout.read().decode('utf-8'))
+#   clientftp = sshclient.open_sftp()
+  
+#   localFile = os.path.join(os.path.dirname(__file__), 'cp3n_1.601.0050.puf')
+
+  # try:
+  #   clientftp.put(localFile, 'firmware/cp3n_1.601.0050.puf', confirm=True)
+  # except Exception as e:
+  #   print(e)
+  #   clientftp.close()
+  #   sshclient.close()
+  #   sys.exit(0)
+
+  # remotepath = clientftp.listdir('firmware')
+  # print(remotepath)
+
+  # clientftp.close()
+  # sshclient.close()
+
+
 def shut_port_down(cmd):
 
   if cmd != 'ctpconsole':
@@ -92,20 +115,31 @@ def runcommands():
       
 
 if __name__ == "__main__":
+  usage = '''
+  usage: python3 main.py [option] <filename>.csv [flag(optional)]
+  -v, --version - get version of devices provided into the csv file
+  -ch, --check-ports - gets the status of ports, returns if the port is on/off for webserver, ftpserver, ctpconsole, telnet
+  -cl, --close-ports - close ports if they are on/enabled for ftpserver, ctpconsole, telnet
+  -a, --adhoc - run a command adhoc
+  '''
 
   args = sys.argv
 
   # WiP for updating systems that are not on the latest firmware
   if len(args) < 3:
     print("Please provide an option for the scan and filename")
+    print(usage)
     sys.exit(0)
   elif len(args) == 4:
     if args[3] == 'all' and (args[1] == '--close-ports' or args[1] == '-cl'):
       shutoff = True
     elif args[3] == 'update':
       update = True
+    elif (args[1] == '--adhoc' or args[1] == '-a') and args[3]:
+      commands = args[3]
     else:
       print(f'{args[3]} is not an option')
+      print(usage)
       sys.exit(0)
 
   try:
@@ -127,6 +161,8 @@ if __name__ == "__main__":
     else:
       commands = ['ver']
     runcommands()
+  elif (args[1] == '--adhoc' or args[1] == '-a') and args[3]:
+    runcommands()
   elif args[1] == '--close-ports' or args[1] == '-cl':
     if shutoff is True:
       commands = ['ftpserver', 'telnet', 'ctpconsole', 'webserver']
@@ -145,6 +181,6 @@ if __name__ == "__main__":
   elif args[1] == '--check-ports' or args[1] == '-ch':
     commands = ['ftpserver', 'telnet', 'ctpconsole', 'webserver']
     runcommands()
-    
+
   else:
     print(f'{args[1]} is not an option')
